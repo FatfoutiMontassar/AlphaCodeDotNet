5702103d84198a96b8533e8b5011b6521e41c927
Montassar FATFOUTI <montassar.fatfouti@outlook.com>
2020-01-21 18:09:19 +0100

    Add project files.

---
+++ AlphaCode/Models/BlogPost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaCode.Models
+{
+	public class BlogPost
+    {
+        [Key]
+        public Guid PostId { get; set; }
+
+        [Required]
+        public string Creator { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Body { get; set; }
+
+        [Required]
+        public DateTime Dt { get; set; }
+    }
+}

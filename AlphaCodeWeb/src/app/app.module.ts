8597a14766e65f2e3813c9278071220f41ed3e62
Montassar FATFOUTI <montassar.fatfouti@outlook.com>
2020-01-22 15:49:48 +0100

    add routing module and other fixes

--- AlphaCodeWeb/src/app/app.module.ts
+++ AlphaCodeWeb/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BlogPostsComponent } from './blog-posts/blog-posts.component';
 import { BlogPostComponent } from './blog-post/blog-post.component';
 import { BlogPostAddEditComponent } from './blog-post-add-edit/blog-post-add-edit.component';
+import { BlogPostService } from './services/blog-post.service';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -14,9 +18,12 @@ import { BlogPostAddEditComponent } from './blog-post-add-edit/blog-post-add-edi
     BlogPostAddEditComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    BlogPostService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
